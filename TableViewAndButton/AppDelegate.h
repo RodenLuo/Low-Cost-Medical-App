e1358c49db7cf5b39c93258498a57003720ef1c6
RodenLuo <Roden@YXtekiMacBook-Air.local>
2014-09-21 11:54:17 -0400

    Initial Commit

---
+++ TableViewAndButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewAndButton
+//
+//  Created by Roden on 9/21/14.
+//  Copyright (c) 2014 Roden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewAndButton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewAndButton
+//
+//  Created by Roden on 9/21/14.
+//  Copyright (c) 2014 Roden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
