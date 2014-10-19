989eeb427692418850dd9e7e425c2635fb585683
RodenLuo <roden.luo@gmail.com>
2014-10-19 11:00:29 +0800

    Commit on Oct 19

---
+++ Podfile
@@ -0,0 +1,4 @@
+inhibit_all_warnings!
+platform :ios, '6.0'
+pod 'WYPopoverController', '~> 0.2.2'
+pod 'BEMSimpleLineGraph'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BEMSimpleLineGraph
+#define COCOAPODS_POD_AVAILABLE_BEMSimpleLineGraph
+#define COCOAPODS_VERSION_MAJOR_BEMSimpleLineGraph 3
+#define COCOAPODS_VERSION_MINOR_BEMSimpleLineGraph 1
+#define COCOAPODS_VERSION_PATCH_BEMSimpleLineGraph 0
+
+// WYPopoverController
+#define COCOAPODS_POD_AVAILABLE_WYPopoverController
+#define COCOAPODS_VERSION_MAJOR_WYPopoverController 0
+#define COCOAPODS_VERSION_MINOR_WYPopoverController 2
+#define COCOAPODS_VERSION_PATCH_WYPopoverController 2
+
